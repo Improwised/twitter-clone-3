0b2865994c4dab751882912109044f5c203aebd2
hemangi <hemangi@improwised.com>
2017-01-18 18:52:53 +0530

    views,css,index.js modified

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,24 @@ const DB = require('../helpers/db');
 const router = express.Router();
 
 // GET: /
-router.get('/', (req, res, next) => {
+
+router.get('/login', (req, res, next) => {
   // Constuct and run a simple query
-  const query = DB.builder()
-    .select()
-    .function('NOW()')
-    .toParam();
+  // const query = DB.builder()
+  //   .select()
+  //   .function('NOW()')
+  //   .toParam();
 
-  DB.executeQuery(query, (error, results) => {
-    if (error) {
-      next(error);
-      return;
-    }
-
-    res.render('index', {
-      title: `Time from the database is ${results.rows[0].now}`,
-    });
-  });
+  // DB.executeQuery(query, (error, results) => {
+  //   if (error) {
+  //     next(error);
+  //     return;
+  //   }
+  res.render('login');
+  //   res.render('index', {
+  //     title: `Time from the database is ${results.rows[0].now}`,
+  //   });
+  // });
 });
 
 module.exports = router;
