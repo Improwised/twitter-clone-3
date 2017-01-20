6de32b67ff46a031eaa6cb7e89e2df4743c9f9c8
hemangi <hemangi@improwised.com>
2017-01-20 13:07:00 +0530

    changes saved

--- routes/index.js
+++ routes/index.js
@@ -4,69 +4,110 @@ const DB = require('../helpers/db');
 
 const router = express.Router();
 
+var logout = require('express-passport-logout');
+var nodemailer = require('nodemailer');
 // GET: /
+
+router.get('/welcome', (req, res, next) => {
+  res.render('welcome');
+})
 
 router.get('/login', (req, res, next) => {
   res.render('login');
 });
 
 router.post('/login', (req, res, next) => {
-  // User.manualLogin(req.body['uid'], req.body['pwd'], function(error, object) {
+
+  var username = req.body.username;
+  var password = req.body.password;
+  // console.log(username + password);
+  // return false;
+  // User.manualLogin(username, password, function(error, object) {
   //   if (!object) {
   //     res.redirect('/login');
   //   } else {
-  //     req.session.uid = object;
+  //     req.session.username = object;
   //     if (req.body['remember_me'] == 'true') {
-  //       res.cookie('uid', object.uid, { maxAge: 900000 });
-  //       res.cookie('pwd', object.pwd, { maxAge: 900000 });
+  //       res.cookie('username', object.username, { maxAge: 900000 });
+  //       res.cookie('password', object.password, { maxAge: 900000 });
   //     }
-  //     res.redirect('/welcome');
+  //     // res.redirect('/welcome');
+  //     res.send('Hello ' + username);
   //   }
   // })
-  var username = req.body.username;
-  var password = req.body.password;
+  // if (username == )
 
-  req.checkBody('username', 'Username is required').notEmpty();
-  req.checkBody('password', 'Password is required').notEmpty();
+  const query = DB.builder()
+    .select()
+    .from("users")
+    .where('username = ?',username)
+    .where('password = ?',password)
+    .toParam();
+    console.log(query);
+    //return false;
+  DB.executeQuery(query, (error, results) => {
 
-  var errors = req.validationErrors();
+   if (error) {
+     next(error);
+     return;
+   }
+   console.log(results.rows.length);
 
-  if(errors) {
-    console.log('FAILED');
-    res.render('register', {
-      errors : errors
-    });
-  } else {
-    // const query = DB.builder()
-    //   .select()
-    //   .from("users")
-    //   .field("username")
-    //   .field("password")
-    //   .toString();
-    // DB.executeQuery(query, (error, results) => {
-
-    //  if (error) {
-    //    next(error);
-    //    return;
-    //  }
-    //  console.log(results);
-    // });
-    res.send('Hello ' + username);
-  }
-
+   if(results.rows.length > 0)
+   {
+    console.log("Log in successfully");
+    res.render('welcome');
+   }
+   else
+   {
+    console.log("Log in Not successfully");
+   }
+   //res.send(results);
+  });
 })
 
 router.get('/retrive_password', (req, res, next) => {
+  res.render('forgot_password');
+});
+
+router.get('/retrive_password', (req, res, next) => {
+
   console.log('email...', req.query.email);
+
+  var smtpTransport = nodemailer.createTransport("SMTP", {
+    service: "Gmail",
+    auth: {
+      user: "hemangi@improwised.com",
+      pass: "hemangi123"
+    }
+  });
+
+  var rand, mailOptions, host, link;
+
   User.getAccountByEmail(req.query.email, function(object) {
     if (object) {
       console.log('email...', object);
       rand = Math.floor((Math.random() * 100) + 54);
       host = req.get('host');
       link = "http://" + req.get('host') + "/verify?id=" + rand;
-
+      mailOptions={
+        to : req.query.email,
+        subject : "Please confirm your Email account",
+        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+      }
+      console.log(mailOptions);
+      smtpTransport.sendMail(mailOptions, function(error, response) {
+        if(error) {
+          console.log(error);
+        } else {
+          console.log("Message sent: " + response.message);
+          res.end("sent");
+        }
+      });
+    } else {
+      res.render('forgot_password');
     }
-  })
+  });
 })
 
 router.get('/', (req, res, next) => {
@@ -108,16 +149,48 @@ router.post('/register', (req, res, next) => {
     .set("mobilenumber", mobileno)
     .set("password", password)
     .toParam();
-     // console.log("-->", query);
+     console.log("-->", query);
     DB.executeQuery(query, (error, results) => {
 
-   if (error) {
-     next(error);
-     return;
-   }
-    res.render('/login')
-  });
+     if (error) {
+       next(error);
+       return;
+     }
+     res.render('register');
+    });
+  // res.send('Hello ' + username + ' Phone ' + mobileno + ' Email ' + email);
   }
 });
 
+router.get('/tweet', (req, res, next) => {
+  res.render();
+});
+
+router.post('/tweet', (req, res, next) => {
+  var tweet = req.body.tweet;
+
+  const query = DB.builder()
+  .insert()
+  .into("tweet")
+  .set("tweet", tweet)
+  .toParam();
+
+  console.log("-->", query);
+
+  DB.executeQuery(query, (error, results) => {
+    if (error) {
+      next(error);
+      return;
+    }
+    res.render('welcome');
+  });
+});
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy(function(e) {
+    res.clearCookie('myCookie');
+    req.logout();
+    res.redirect('/login');
+  });
+});
 module.exports = router;
